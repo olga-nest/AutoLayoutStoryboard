0167359e1b31d677dd62f2844dcd618053acc64a
olga-nest <olga.w.nesterova@gmail.com>
2017-10-10 11:19:34 -0700

    Initial Commit

---
+++ AutoLayoutStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryboard
+//
+//  Created by Olga on 10/10/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
